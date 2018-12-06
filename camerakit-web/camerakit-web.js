ea712e771b02d82a1e8c8ff4deb528a6b193e5dd
Kyle <Timone@users.noreply.github.com>
2018-12-06 13:46:23 -0500

    Add video stream and download example

--- camerakit-web/camerakit-web.js
+++ camerakit-web/camerakit-web.js
@@ -1,28 +1,32 @@
 import adapter from 'webrtc-adapter';
 
-const handleError = (error) => {
-  console.log('Error: ', error);
-};
+console.log(`Using webrtc-adapter ${adapter.extractVersion}`);
 
+let recordedBlobs = [];
+let mediaRecorder;
 const CameraKitWeb = {
   getDevices: async () => {
     const audioSources = [];
     const videoSources = [];
     await navigator.mediaDevices.enumerateDevices().then((devices) => {
-      devices.forEach(device => {
-        switch(device.kind) {
+      devices.forEach((device) => {
+        switch (device.kind) {
           case 'audioinput':
-            audioSources.push({ device, label: device.label || `audioinput` });
+            audioSources.push({ device, label: device.label || 'audioinput' });
             break;
           case 'videoinput':
-            videoSources.push({ device, label: device.label || `videoinput` });
+            videoSources.push({ device, label: device.label || 'videoinput' });
+            break;
+          default: console.log('Other input type detected.');
         }
-      })
+      });
     });
     return { audioSources, videoSources };
   },
 
-  initializeCamera: ({ videoElement, audioDeviceId, videoDeviceId, height, width, ideal, max, facingMode = 'environment' }) => {
+  initializeCamera: ({
+    audioDeviceId, videoDeviceId, height, width, ideal, max, facingMode = 'environment',
+  }) => {
     const constraints = {
       audio: {
         deviceId: { exact: audioDeviceId },
@@ -38,12 +42,7 @@ const CameraKitWeb = {
         facingMode,
       },
     };
-    const gotStream = (stream) => {
-      window.stream = stream;
-      videoElement.srcObject = stream;
-      videoElement.play();
-    };
-    navigator.mediaDevices.getUserMedia(constraints).then(gotStream).catch(handleError);
+    return navigator.mediaDevices.getUserMedia(constraints);
   },
 
   takePicture: ({ videoElement }) => {
@@ -55,6 +54,40 @@ const CameraKitWeb = {
     canvas.height = videoElement.videoHeight;
     context.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
     return canvas.toDataURL('image/png');
-  }
-}
+  },
+
+  startRecording: ({ stream }) => {
+    const mediaSource = new MediaSource();
+    recordedBlobs = [];
+
+    const handleDataAvailable = (event) => {
+      if (event.data && event.data.size > 0) {
+        recordedBlobs.push(event.data);
+      }
+    };
+    mediaSource.addEventListener('sourceopen', () => {
+      mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+    }, false);
+    try {
+      mediaRecorder = new MediaRecorder(stream, { mimeType: 'video/webm;codecs=vp8' });
+    } catch (e) {
+      console.error('Exception while creating MediaRecorder:', e);
+      return;
+    }
+    console.log('Created MediaRecorder', mediaRecorder);
+    mediaRecorder.onstop = (event) => {
+      console.log('Recorder stopped: ', event);
+    };
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start(10); // collect 10ms of data
+    console.log('MediaRecorder started', mediaRecorder);
+  },
+
+  stopRecording: () => {
+    mediaRecorder.stop();
+    return new Blob(recordedBlobs, { type: 'video/webm' });
+  },
+};
+
+
 export default CameraKitWeb;
--- pages/index.js
+++ pages/index.js
@@ -9,41 +9,82 @@ class Example extends React.Component {
       videoSources: [],
       image: undefined,
       imageTaken: false,
-    }
+      stream: undefined,
+      videoTaken: undefined,
+    };
+  }
+
+  handleError = (error) => {
+    console.log('Error: ', error);
   }
 
   getDevices = () => {
     CameraKitWeb.getDevices().then(({ audioSources, videoSources }) => {
       this.setState({ audioSources, videoSources });
-    }).catch(error => console.log(error));
+    }).catch(this.handleError);
+  };
+
+  gotStream = (stream) => {
+    this.setState({ stream });
+    this.src.srcObject = stream;
+    this.src.play();
   };
 
   requestCamera = () => {
     CameraKitWeb.initializeCamera({
-      videoElement: this.video,
       audioDeviceId: this.audioSource.value,
       videoDeviceId: this.videoSource.value,
-    });
+    }).then(this.gotStream).catch(this.handleError);
   }
 
   takePicture = () => {
     this.setState({
       imageTaken: true,
       image: CameraKitWeb.takePicture({
-        videoElement: this.video,
+        videoElement: this.src,
       }),
     }, this.showPicture);
   }
 
   showPicture = () => {
-    console.log(this.imageContainer);
-    this.imageContainer.src = this.state.image;
+    const { image } = this.state;
+    this.imageContainer.src = image;
   }
 
   downloadPicture = () => {
     const a = document.createElement('a');
-    a.download = `CKW-${new Date}`;
-    a.href = this.state.image;
+    const { image } = this.state;
+    a.download = `CKW-${new Date()}`;
+    a.href = image;
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+
+  startRecording = () => {
+    const { stream } = this.state;
+    CameraKitWeb.startRecording({ stream });
+    this.setState({ recording: true });
+  }
+
+  stopRecording = () => {
+    const buffer = CameraKitWeb.stopRecording();
+    this.setState({ video: buffer, recording: false, videoTaken: true }, () => {
+      const { video } = this.state;
+      this.out.src = null;
+      this.out.srcObject = null;
+      this.out.src = window.URL.createObjectURL(video);
+      this.out.controls = true;
+      this.out.play();
+    });
+  }
+
+  downloadVideo = () => {
+    const a = document.createElement('a');
+    const { video } = this.state;
+    a.download = `CKW-${new Date()}`;
+    a.href = window.URL.createObjectURL(video);
     a.style.display = 'none';
     document.body.appendChild(a);
     a.click();
@@ -51,30 +92,38 @@ class Example extends React.Component {
   }
 
   render() {
+    const {
+      audioSources, videoSources, imageTaken, recording, videoTaken,
+    } = this.state;
     return (
       <div>
-        <button onClick={this.getDevices}>Get Audio/Video Devices</button>
-        <select ref={select => this.audioSource = select} >
+        <button type="button" onClick={this.getDevices}>Get Audio/Video Devices</button>
+        <select ref={(select) => { this.audioSource = select; }}>
           {
-            this.state.audioSources.map(source => (
+            audioSources.map(source => (
               <option key={source.label} value={source.device.deviceId}>{source.label}</option>
             ))
           }
         </select>
-        <select ref={select => this.videoSource = select }>
+        <select ref={(select) => { this.videoSource = select; }}>
           {
-            this.state.videoSources.map(source => (
+            videoSources.map(source => (
               <option key={source.label} value={source.device.deviceId}>{source.label}</option>
             ))
           }
         </select>
         <br />
-        <button onClick={this.requestCamera}>Request Audio/Video Stream</button>
-        <video ref={video => (this.video = video)}></video>
+        <button type="button" onClick={this.requestCamera}>Request Audio/Video Stream</button>
+        <video ref={(video) => { this.src = video; }} />
         <br />
-        <button onClick={this.takePicture}>Take Picture</button>
-        {this.state.imageTaken && (<img ref={img => this.imageContainer = img}></img>)}
-        {this.state.imageTaken && (<button onClick={this.downloadPicture}>Download Image</button>)}
+        <button type="button" onClick={this.takePicture}>Take Picture</button>
+        {imageTaken && (<img alt="capture of webcam screen" ref={(img) => { this.imageContainer = img; }} />)}
+        {imageTaken && (<button type="button" onClick={this.downloadPicture}>Download Image</button>)}
+        <br />
+        <button type="button" disabled={recording} onClick={this.startRecording}>Start Recording</button>
+        <button type="button" disabled={!recording} onClick={this.stopRecording}>Stop Recording</button>
+        {videoTaken && (<video ref={(video) => { this.out = video; }} />)}
+        {videoTaken && (<button type="button" disabled={recording} onClick={this.downloadVideo}>Download Video</button>)}
       </div>
     );
   }
